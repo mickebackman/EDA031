586a7e2e6f6de3aa7ba73decbcfcbe9243e1ddf6
FredrikKaraker <fredrik.k.sundstrom@gmail.com>
2016-04-06 14:28:20 +0200

    diskdatabase snart klar

--- clientserver/diskdatabase.h
+++ clientserver/diskdatabase.h
@@ -1,3 +1,39 @@
-// Shell file
+#ifndef DISKDATABASE_H
+#define DISKDATABASE_H
 
-// Class should be named DiskDatabase, empty constructor.
+#include <map>
+#include <string>
+#include "database.h"
+#include "newsgroup.h"
+#include "article.h"
+#include <vector>
+#include <stdlib.h>
+#include <dirent.h>
+
+//Every directory includes a meta file. In the root directory the file includes
+//the next group id followed by the number if news groups. In the news groups it
+// includes the next article id, the number of articles and the name of the news group.
+
+class DiskDatabase : public Database{
+
+public:
+   DiskDatabase();
+   ~DiskDatabase();
+   void addArticle(int newsGroupId, std::string name, std::string author, std::string text);
+   void addNewsGroup(std::string newsGroupName);
+   Article getArticle(int newsGroupId, int articleId);
+   void deleteArticle(int newsGroupId, int articleId);
+   void deleteNewsGroup(int newsGroupId);
+   std::vector<std::pair<int, std::string> > getNewsGroups();
+   std::map<int, Article> getArticlesInNewsGroup(int newsGroupId);
+   int numberOfNewsGroups();
+   int numberOfArticlesInNewsGroup(int newsGroupId);
+
+ private:
+   // The root directory.
+   string root;
+   int numberOfNewsGroups;
+
+};
+
+#endif
